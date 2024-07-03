cc7b4838e4cd09a2a092826180e21a64b6028c33
IsThe0ne <flasproj@gmail.com>
2024-07-03 15:29:00 +0200

    edit api.py for redirect logo

--- api.py
+++ api.py
@@ -163,6 +163,61 @@ def get_tracking_data(campaign_id):
 
 @app.route('/logo.jpg')
 def serve_logo():
+
+    is_user_exist = False
+
+    data = request.args
+    tracking_id = data['tracking_id']
+    campaign_id = data['campaign_id']
+    group_id = data['group_id']
+    user_id = data['user_id']
+    end_date = str(data['end_date'])
+    admin_id = data['admin_id']
+    timestamp = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    event = 'open'
+    if timestamp <= end_date:
+        try:
+            with open(data_file) as sd:
+                stored_data = json.load(sd)
+
+            if len(stored_data) != 0:
+
+                if mail_campaign_open not in stored_data:
+                        stored_data[mail_campaign_open] = {'data': []}
+                else:
+                    for i in stored_data[mail_campaign_open]['data']:
+                        # Check if the user exist
+                        if i['user_id'] == user_id :
+                            is_user_exist = True
+                            print("user exist")
+                            break
+
+                # Closing file
+                sd.close()
+
+                if not is_user_exist:
+                    print("user not exist")
+                    
+
+                    stored_data[mail_campaign_open]['data'].append({'user_id': user_id, 'campaign_id': campaign_id, 'group_id': group_id, 'event': event, 'timestamp': timestamp, 'admin_id': admin_id})
+
+            elif mail_campaign_open not in stored_data:
+                print("Key not exist")
+                stored_data[mail_campaign_open] = {'data': []}
+                stored_data[mail_campaign_open]['data'].append({'user_id': user_id, 'campaign_id': campaign_id, 'group_id': group_id, 'event': event, 'timestamp': timestamp, 'admin_id': admin_id})
+
+            with open(data_file, 'w') as f:
+                json.dump(stored_data, f)
+
+
+        except json.JSONDecodeError:
+            if mail_campaign_open not in tracking_data:
+                tracking_data[mail_campaign_open] = {'data': []}
+                tracking_data[mail_campaign_open]['data'].append({'user_id': user_id, 'campaign_id': campaign_id, 'group_id': group_id, 'event': event, 'timestamp': timestamp, 'admin_id': admin_id})
+
+            with open(data_file, 'w') as f:
+                json.dump(tracking_data, f)
+
     return send_from_directory(app.root_path, 'logo.jpg')
 
 if __name__ == '__main__':
