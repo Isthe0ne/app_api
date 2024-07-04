4cb5d551efe2f8b1226ba61b2fd8aa1f2b713cc2
IsThe0ne <flasproj@gmail.com>
2024-07-04 21:59:39 +0200

    API Modifications - Integration PostgreSQL

--- api.py
+++ api.py
@@ -1,35 +1,35 @@
-from flask import Flask, request, jsonify, send_from_directory, redirect
-from sqlalchemy import create_engine, text
 import os
+from flask import Flask, request, jsonify, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, text
 import datetime
 import json
 
 app = Flask(__name__)
 
-SSL_CA = '/etc/secrets/singlestore_bundle.pem'
+# Use environment variables for configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize the database
+db = SQLAlchemy(app)
 
-USERNAME = os.environ.get('USERNAME')
-PASSWORD = os.environ.get('PASSWORD')
-HOST = os.environ.get('HOST')
-PORT = os.environ.get('PORT')
-DB = os.environ.get('DB')
+# Create Group Model
+class TrackingDataMail(db.Model):
+    __tablename__ = 'tracking_data_mail'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    campaign_id = db.Column(db.Integer, nullable=False)
+    group_id = db.Column(db.Integer, nullable=False)
+    event_open = db.Column(db.Boolean)
+    event_click = db.Column(db.Boolean)
+    event_phished = db.Column(db.Boolean)
+    timestamp_open = db.Column(db.DateTime)
+    timestamp_click = db.Column(db.DateTime)
+    timestamp_phished = db.Column(db.DateTime)
+    admin_id = db.Column(db.String(255), nullable=False)
 
-
-# Connect to the SingleStore database
-engine = create_engine(f'mysql+mysqldb://{USERNAME}:{PASSWORD}@{HOST}/{DB}?ssl_ca={SSL_CA}')
-# Create the connection
-conn = engine.connect()
-
-
-data_file = 'tracking_data.json'
-
-tracking_data = {}
-mail_campaign_open = "mail_campaign_open"
-mail_campaign_click = "mail_campaign_click"
-
-
-
+#Track for clicks
 @app.route('/track/click', methods=['GET'])
 def track_click():
     data = request.args
@@ -38,63 +38,55 @@ def track_click():
     if not all(key in data for key in ['tracking_id', 'campaign_id', 'group_id', 'user_id', 'end_date', 'admin_id']):
         return 'Missing input data', 400
 
-    tracking_id = data['tracking_id']
-    campaign_id = data['campaign_id']
-    group_id = data['group_id']
-    user_id = data['user_id']
-    end_date = str(data['end_date'])
-    admin_id = data['admin_id']
-
-    # Validate input data types
     try:
-        campaign_id = int(campaign_id)
-        group_id = int(group_id)
-        user_id = int(user_id)
+        campaign_id = int(data['campaign_id'])
+        group_id = int(data['group_id'])
+        user_id = int(data['user_id'])
     except ValueError:
         return 'Invalid input data', 400
 
-    # Validate input data ranges
     if campaign_id < 0 or group_id < 0 or user_id < 0:
         return 'Invalid input data', 400
 
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    end_date = str(datetime.datetime.strptime(data['end_date'], "%Y-%m-%d %H:%M:%S"))
+    admin_id = data['admin_id']
 
     if timestamp <= end_date:
-        # Check if the user already exists in the table
-        with engine.connect() as conn:
+        with db.engine.connect() as conn:
             result = conn.execute(text("""
             SELECT * FROM tracking_data_mail
             WHERE user_id = :user_id AND campaign_id = :campaign_id AND group_id = :group_id
             """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id}).fetchone()
 
             if result is None:
-                # Insert the data into the table
                 conn.execute(text("""
                 INSERT INTO tracking_data_mail (user_id, campaign_id, group_id, event_click, timestamp_click, admin_id)
                 VALUES (:user_id, :campaign_id, :group_id, TRUE, :timestamp, :admin_id)
                 """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id, "timestamp": timestamp, "admin_id": admin_id})
             else:
-                # Check if the user clicked event already set to TRUE
+
                 result = conn.execute(text("""
                 SELECT * FROM tracking_data_mail
                 WHERE user_id = :user_id AND campaign_id = :campaign_id AND group_id = :group_id AND event_click = TRUE
                 """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id}).fetchone()
 
                 if result is None:
-                    # Update the data in the table
                     conn.execute(text("""
                     UPDATE tracking_data_mail
                     SET event_click = TRUE, timestamp_click = :timestamp
                     WHERE user_id = :user_id AND campaign_id = :campaign_id AND group_id = :group_id
-                    """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id, "timestamp": timestamp})
+                    """), {"timestamp": timestamp, "user_id": user_id, "campaign_id": campaign_id, "group_id": group_id})
+
                 else:
-                    print("exist!")
+                    print("exist!")   
 
             conn.commit()
 
     return '', 204
 
 
+#Track for opens
 @app.route('/logo.jpg')
 def serve_logo():
     data = request.args
@@ -103,72 +95,64 @@ def serve_logo():
     if not all(key in data for key in ['tracking_id', 'campaign_id', 'group_id', 'user_id', 'end_date', 'admin_id']):
         return 'Missing input data', 400
 
-    tracking_id = data['tracking_id']
-    campaign_id = data['campaign_id']
-    group_id = data['group_id']
-    user_id = data['user_id']
-    end_date = str(data['end_date'])
-    admin_id = data['admin_id']
-
-    # Validate input data types
     try:
-        campaign_id = int(campaign_id)
-        group_id = int(group_id)
-        user_id = int(user_id)
+        campaign_id = int(data['campaign_id'])
+        group_id = int(data['group_id'])
+        user_id = int(data['user_id'])
     except ValueError:
         return 'Invalid input data', 400
 
-    # Validate input data ranges
     if campaign_id < 0 or group_id < 0 or user_id < 0:
         return 'Invalid input data', 400
 
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    end_date = str(datetime.datetime.strptime(data['end_date'], "%Y-%m-%d %H:%M:%S"))
+    admin_id = data['admin_id']
 
     if timestamp <= end_date:
-        # Check if the user already exists in the table
-        with engine.connect() as conn:
+        with db.engine.connect() as conn:
             result = conn.execute(text("""
             SELECT * FROM tracking_data_mail
             WHERE user_id = :user_id AND campaign_id = :campaign_id AND group_id = :group_id
             """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id}).fetchone()
 
             if result is None:
-                # Insert the data into the table
                 conn.execute(text("""
                 INSERT INTO tracking_data_mail (user_id, campaign_id, group_id, event_open, timestamp_open, admin_id)
                 VALUES (:user_id, :campaign_id, :group_id, TRUE, :timestamp, :admin_id)
                 """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id, "timestamp": timestamp, "admin_id": admin_id})
             else:
-                # Check if the user opened event already set to TRUE
                 result = conn.execute(text("""
                 SELECT * FROM tracking_data_mail
                 WHERE user_id = :user_id AND campaign_id = :campaign_id AND group_id = :group_id AND event_open = TRUE
                 """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id}).fetchone()
 
+
                 if result is None:
-                    # Update the data in the table
                     conn.execute(text("""
                     UPDATE tracking_data_mail
                     SET event_open = TRUE, timestamp_open = :timestamp
                     WHERE user_id = :user_id AND campaign_id = :campaign_id AND group_id = :group_id
-                    """), {"user_id": user_id, "campaign_id": campaign_id, "group_id": group_id, "timestamp": timestamp})
+                    """), {"timestamp": timestamp, "user_id": user_id, "campaign_id": campaign_id, "group_id": group_id})
+
                 else:
                     print("exist!")
 
             conn.commit()
+
     return send_from_directory(app.root_path, 'logo.jpg')
 
 
-@app.route('/get_tracking_data/<campaign_id>', methods=['GET', 'POST'])
+#Get data from DB
+@app.route('/get_tracking_data/<campaign_id>', methods=['GET'])
 def get_tracking_data(campaign_id):
 
+    campaign_id = str(campaign_id)
     clicked_users = []
     opened_users = []
     all_users = []
 
-    campaign_id = str(campaign_id)
-
-    with engine.connect() as conn:
+    with db.engine.connect() as conn:
         result = conn.execute(text("""
         SELECT * FROM tracking_data_mail
         WHERE campaign_id = :campaign_id
@@ -194,15 +178,13 @@ def get_tracking_data(campaign_id):
                 clicked_users.append(user)
             if row[5]:
                 opened_users.append(user)
-            
 
-    if len(clicked_users) != 0 or len(opened_users) != 0 or len(all_users) != 0:
-        return jsonify({'clicked_users': clicked_users, 'opened_users': opened_users, 'all_users' : all_users})
+    if clicked_users or opened_users or all_users:
+        return jsonify({'clicked_users': clicked_users, 'opened_users': opened_users, 'all_users': all_users})
     else:
         return '', 404
 
 
 
-
 if __name__ == '__main__':
-   app.run(debug=True, port=5050)
+    app.run(debug=True, port=5050)
